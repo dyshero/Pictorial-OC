d82029846c4d3f92be372a37e16bdeec0639bec7
dyshero <herodys@163.com>
2017-05-31 15:37:24 +0800

    初始化项目

---
+++ Pictorial-OC/Pictorial-OC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Pictorial-OC
+//
+//  Created by duodian on 2017/5/31.
+//  Copyright © 2017年 duodian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
