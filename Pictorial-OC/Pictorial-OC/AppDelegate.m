c9105e33f06c5c71723b30aa669c2b1f6cc010dc
丁远帅 <yuanshuai.ding@duodian.com>
2017-10-31 15:36:43 +0800

    no message

--- Pictorial-OC/Pictorial-OC/AppDelegate.m
+++ Pictorial-OC/Pictorial-OC/AppDelegate.m
@@ -33,6 +33,8 @@
 
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
+    NSLog(@"..........----=====。。。");
+
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
 }
@@ -49,6 +51,7 @@
 
 
 - (void)applicationWillTerminate:(UIApplication *)application {
+    NSLog(@"..........----=====");
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
 
