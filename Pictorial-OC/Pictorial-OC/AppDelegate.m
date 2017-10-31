3606f5867441adf4f28c6f1debdf5c7108223392
丁远帅 <yuanshuai.ding@duodian.com>
2017-10-31 16:24:35 +0800

    no message

--- Pictorial-OC/Pictorial-OC/AppDelegate.m
+++ Pictorial-OC/Pictorial-OC/AppDelegate.m
@@ -17,6 +17,7 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    // 暂存测试
     self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
